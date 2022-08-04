e7cc1ccc3e175274606113061896af29684b5a71
David Ben-Adeji <benadejidavid@gmail.com>
2022-08-04 14:01:15 +0100

    completed first task

--- _printf.c
+++ _printf.c
@@ -2,31 +2,28 @@
 #include <stdarg.h>
 #include <stdlib.h>
 #include <stdio.h>
-
 /**
 * _printf - function to print a string
 * @format: the string along with its flags
 * Return: the lenght of the string
 */
-
 int _printf(const char *format, ...)
 {
-	int i, len = _strlen(format), aux_len = len;
+	int i, len = _strlen(format), r_len = 0;
 	va_list args;
 
 	if (format == NULL || *format == '\0')
-		return (-1);
+		return (0);
 
 	va_start(args, format);
 
-	for (i = 0; i < aux_len; i++)
+	for (i = 0; i < len; i++)
 	{
 		if (format[i] == '%' && format[i + 1] == 'c')
 		{
 			char c = (char) va_arg(args, int);
-
 			putchar(c);
-			len--;
+			r_len++;
 			i++;
 			continue;
 		}
@@ -35,12 +32,13 @@ int _printf(const char *format, ...)
 			char *s = va_arg(args, char *);
 
 			print_string(s);
+			r_len += _strlen(s);
 			i++;
-			len--;
 			continue;
 		}
 		putchar(format[i]);
+		r_len++;
 	}
 	va_end(args);
-	return (len);
+	return (r_len);
 }
